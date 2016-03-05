f60a31932a4c3f433beeff111fd32364ece578ad
Alexey Drozdov <alexey.drozdov@gmail.com>
2016-03-05 13:06:30 +0300

    Add colorizr

--- lib/jury.rb
+++ lib/jury.rb
@@ -1,3 +1,5 @@
+require 'colorizr'
+
 class Jury
 	attr_accessor :members
 
@@ -15,7 +17,8 @@ class Jury
 		finalists.each {|finalist| finalists_hash[finalist] = 0}
 
 		# puts each member
-		@members.each {|member| puts "Member: #{member}"}
+		puts "*"*20
+		@members.each {|member| puts "Member: #{member.to_s.green}"}
 
 		# vote for finalists
 		keys = finalists_hash.keys
@@ -26,12 +29,14 @@ class Jury
 	end
 
 	def report_votes(final_votes)
-		final_votes.each {|key, value| puts "Finalist #{key}: #{value}"}
+		puts "*"*20
+		final_votes.each {|key, value| puts "Finalist #{key.to_s.blue}: #{value.to_s.pink}"}
 	end
 
 	def announce_winner(final_votes)
 		winner = final_votes.max_by{|key, value| final_votes[key]}[0]
-		puts "The winner iiiiis: #{winner}"
+		puts "*"*20
+		puts "The winner iiiiis: #{winner.to_s.red}"
 		winner
 	end
 end
